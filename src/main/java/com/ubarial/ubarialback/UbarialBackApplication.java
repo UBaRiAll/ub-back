6a2a873937fed2dfb883b6f72eb7183f81d6d111
bonooy <choiboyuna@gmail.com>
2024-02-28 11:10:07 +0900

    dev test commit

--- src/main/java/com/ubarial/ubarialback/UbarialBackApplication.java
+++ src/main/java/com/ubarial/ubarialback/UbarialBackApplication.java
@@ -2,7 +2,7 @@ package com.ubarial.ubarialback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class UbarialBackApplication {
 
